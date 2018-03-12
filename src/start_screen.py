f1c67107950176e7a126a0ea7db95489546ef8ba
Volodymyr Varchuk <fildes@gmail.com>
2018-03-12 13:48:19 -0500

    added recent projects list

--- src/start_screen.py
+++ src/start_screen.py
@@ -1,12 +1,13 @@
 __author__ = 'Vladimir'
 
-from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ObjectProperty, DictProperty
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ObjectProperty, DictProperty, ListProperty
 from kivy.uix.screenmanager import Screen
 from kivy.app import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from os import path as p
 
+recent_projetcs_file = 'recent_projects.ini'
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
@@ -24,6 +25,39 @@ class StartScreen(Screen):
     loadfile = ObjectProperty(None)
     savefile = ObjectProperty(None)
     text_input = ObjectProperty(None)
+    project_list = ListProperty(None)
+
+    # def __init__(self):
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        if p.exists(recent_projetcs_file):
+            with open(recent_projetcs_file) as f:
+                self.project_files = f.read().splitlines()
+        else:
+            self.project_files = []
+        if len(self.project_files) > 0:
+            self.ids.st_layout.clear_widgets()
+            for project_file in self.project_files:
+                # layout_size = self.ids.st_layout.size
+                # text_widht = len(field_item) * 9 + 20
+                self.ids.st_layout.add_widget(Builder.load_string('''
+Button:
+    text: '{project_file_name}'
+    size_hint_y: None
+    height: 40
+    on_release: app.go_to_project(self.text)
+    '''.format(project_file_name=project_file)))
+        # on_release: root.go_to_project(self.text)
+        # input_paths = []
+        # selection_keys_lower = [p.basename(selection_item) for selection_item in self.selection.keys()]
+        # for workflow_item in self.project.project['workflow']:
+        #     if workflow_item['title'].lower() in selection_keys_lower:
+        #         input_paths.append(workflow_item['output'])
+        # print(input_paths)
+        # if not refresh_source:
+        #     self.ids.ti_input_objects.text = ', '.join(
+        #         [selection_values[0] for selection_values in self.selection.values()])
+
     def dismiss_popup(self):
         self._popup.dismiss()
 
